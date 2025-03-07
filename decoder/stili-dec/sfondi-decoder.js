b22ccc09657808bf81a263b5db3e99cb3790b085
brycinema <46980007+brymeteo@users.noreply.github.com>
2025-03-07 21:02:49 +0100

    Create sfondi-decoder.js

---
+++ decoder/stili-dec/sfondi-decoder.js
@@ -0,0 +1,90 @@
+
+
+<!-- sfondo Altri Canali -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function channelsName() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/altricanali.webp')";
+  }
+<!-- sfondo Guida TV sezioni -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function channeguidtvNome() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/home.webp')";
+  }
+<!-- sfondo App e Servizi -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function appservNome() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/app-servizi.webp')";
+  }
+<!-- sfondo Intrattenimento -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function intrattenimeNome() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/intrattenimento.webp')";
+  }
+<!-- sfondo Bambini -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function neoKidsName() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/bambinik.webp')";
+  }
+<!-- sfondo On Demand -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function ondemandFunzioneNome() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/ondemand.webp')";
+  }
+<!-- sfondo Cinema -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function nuovaFunzioneNome() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/cinema.webp')";
+  }
+<!-- sfondo Assistenza e risorse -->
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function newFunctionName() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/app-servizi.webp')";
+  }
+<!-- sfondo Cartoni Animati -->
+  // Funzione per cambiare lo sfondo quando si clicca sul pulsante "Cartoni animati"
+  function changeBackground() {
+    // Ripristina lo sfondo predefinito prima di cambiare lo sfondo
+    resetBackground();
+    // Imposta il nuovo sfondo
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/sezionebambini.webp')";
+  }
+<!-- sfondo Home normale -->
+  // Funzione per ripristinare lo sfondo predefinito
+  function resetBackground() {
+    document.body.style.backgroundImage = "url('https://brymeteo.github.io/player/decoder/immagini/home3.webp')";
+  }
+// Funzione per cambiare lo sfondo quando si clicca sul pulsante "Guida TV"
+  function changeBackgroundGuidaTV() {
+    resetBackground(); // Ripristina lo sfondo predefinito
+    // Se necessario, puoi impostare un altro sfondo per questo pulsante
+  }
+
+  // Funzione per cambiare lo sfondo quando si clicca su altri pulsanti (aggiungi altre funzioni se necessario)
+  function changeBackgroundAltroPulsante() {
+    resetBackground(); // Ripristina lo sfondo predefinito
+    // Se necessario, puoi impostare un altro sfondo per altri pulsanti
+  }
