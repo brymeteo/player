cc2944dd6d7bcf181e3ff94f035eb025e39878e3
brycinema <46980007+brymeteo@users.noreply.github.com>
2025-06-13 18:47:00 +0200

    Add files via upload

---
+++ Music-Player-main/music-list.js
@@ -0,0 +1,113 @@
+let allMusic = [
+    {
+        name: "Bara Bada Bastu",
+        artist: "KAJ",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Bara Bada Bastu Instrumental",
+        artist: "KAJ",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "In The Air Tonight Instrumental",
+        artist: "Phil Collins",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Double Life",
+        artist: "Pharrell Williams",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Could You Be Loved",
+        artist: "Bob Marley",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Pitbull Mashup",
+        artist: "Pitbull",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "You're My Heart, You're My Soul",
+        artist: "Modern Talking",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Brother Louie",
+        artist: "Modern Talking",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Cheri Cheri Lady",
+        artist: "Modern Talking",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Touch in the Night ",
+        artist: "Radio Version",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Espresso Macchiato Instrumental",
+        artist: "Tommy Cash",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Rockabye",
+        artist: "Clean Bandit ft. Sean Paul & Anne-Marie",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "In The Air Tonight",
+        artist: "Phil Collins",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "4 Minutes",
+        artist: "Madonna, Justin Timberlake",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "The Way I Are",
+        artist: "Timbaland",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "The Way I Are Instrumental",
+        artist: "Timbaland",
+        img: "music-10",
+        src: "music-10"
+    },
+    
+    {
+        name: "Bloody Mary Instrmental",
+        artist: "Lady Gaga",
+        img: "music-11",
+        src: "music-11"
+    },
+    
+    {
+        name: "Alejandro Instrmental",
+        artist: "Lady Gaga",
+        img: "music-12",
+        src: "music-12"
+    },
+    
+];
