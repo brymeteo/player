56bceb193b62d619b43f2bc46df09e921b1f04a7
brycinema <46980007+brymeteo@users.noreply.github.com>
2026-05-06 00:14:15 +0200

    Update timer-zapping.js

--- canali-tv-nazionali/canali-zapping/timer-zapping.js
+++ canali-tv-nazionali/canali-zapping/timer-zapping.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('background-overlay').style.display = "none"; // Nasconde lo sfondo dopo 3 secondi
         document.getElementById('player').classList.remove('hidden'); // Mostra il player video
         player.play();
-    }, 3000);
+    }, 4000);
 });
